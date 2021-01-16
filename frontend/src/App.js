3d553e97fe70feac9b9f61b4edb3481f13b88337
Andrew Arderne <andrew.arderne@protonmail.com>
2021-01-16 14:11:41 +0200

    added react code

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "./Graph";
+import Graph from "./Graph";
+import Button from "./Button";
+
+export default function App() {
+  const [dis, setDis] = useState(<Button></Button>);
+  const url = "/api/transactions";
+  let fetchAns = async () => {
+    let res = await fetch(url);
+    res = await res.json();
+    console.log(res.user_has_data);
+    if (res.user_has_data === true) {
+      setDis(<Graph data={res.transactions}></Graph>);
+    }
+  };
+  useEffect(() => {
+    fetchAns();
+  }, []);
+  return <div className="primary">{dis}</div>;
+}
---
+++ frontend/src/Button.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+function Button() {
+    return (
+        <a className='button' href='/truelayer_signin'>Connect to your bank</a>
+    )
+}
+
+export default Button;
