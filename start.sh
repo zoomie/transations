dcff111ceca47fa1bcdd3e30ea5c0445548a5836
Andrew Arderne <andrew.arderne@protonmail.com>
2021-01-22 14:37:14 +0200

    connect nginx and uwsgi:flask using docker compose over docker internal network

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull
-imageName="transactions_image"
-containerName="running_transactions"
-docker build -t ${imageName} .
-docker rm --force ${containerName}
-docker run -p 80:80 --name=${containerName} ${imageName}
-
